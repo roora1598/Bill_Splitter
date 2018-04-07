1a3ae527d52f71bfc4059945cd2042241532560c
roora1598 <ruchap.ranade@gmail.com>
2018-04-07 22:32:20 +0530

    Initial

---
+++ app/src/main/java/com/example/roora1598/bill_splitter/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.roora1598.bill_splitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
